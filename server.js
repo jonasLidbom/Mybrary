161da2b390bfbb2e7ca9baee9bef6b4107eadf30
Jonas Lidbom <lidbomj@gmail.com>
2022-08-09 12:49:34 +0200

    Procfile

--- server.js
+++ server.js
@@ -22,4 +22,6 @@ db.once('open', () => console.error('Connected to Mongoose'))
 
 app.use('/', indexRouter);
 
-app.listen(process.env.Port || 3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
